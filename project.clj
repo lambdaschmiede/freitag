44b1410956a271ffeec3bbb1b17ed11816f3a69f
Tim Zöller <tim.zoeller@lambdaschmiede.com>
2021-10-08 00:04:05 +0200

    Fixed resource lookup issue

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.lambdaschmiede/freitag "0.1.0"
+(defproject com.lambdaschmiede/freitag "0.1.1-SNAPSHOT"
   :description "A library for public holidays"
   :url "https://github.com/lambdaschmiede/freitag"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
