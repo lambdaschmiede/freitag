2e6a8457a277c7d36f6f3f438f56154fe121cd1a
Tim Zöller <tim.zoeller@lambdaschmiede.com>
2021-10-07 17:31:23 +0200

    Added test

--- test/freitag/core_test.clj
+++ test/freitag/core_test.clj
@@ -1,7 +1,14 @@
 (ns freitag.core-test
-  (:require [clojure.test :refer :all]
-            [freitag.core :refer :all]))
+  (:require [clojure.test :refer [deftest testing is]]
+            [freitag.core :refer [query]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest query-test
+  (testing "No filter only shows nation-wide holidays"
+    (let [result (query {:country :de, :year 2021, :month 1})]
+      (is (= 1 (count result)))
+      (is (= "Neujahr" (:name (first result))))))
+
+  (testing "Filters show nation-wide and regional holidays"
+    (let [result (query {:country :de, :year 2021, :month 1, :state :bw})]
+      (is (= 2 (count result)))
+      (is (= '("Neujahr" "Heilige drei Könige") (map :name result))))))
